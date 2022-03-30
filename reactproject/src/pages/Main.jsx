97a976f8baa6f80e7f9d1bcec92f74cd6ef6ed34
Nikita Zhamoidzik <gorobot.nz@gmail.com>
2022-03-30 21:58:54 +0300

    Fix classes

--- reactproject/src/pages/Main.jsx
+++ reactproject/src/pages/Main.jsx
@@ -2,40 +2,40 @@ import React from "react";
 
 const Main = () => {
     return (
-        <main class="main">
-            <div class="container">
-                <section class="user-container">
-                    <div id="user-avatar-container" class="user-container__avatar-container">
-                        <img class="shadow" id="user-avatar" src="./img/c46888f9135c3b440f5d9060a345b35e.png" alt="No image yet" />
+        <main className="main">
+            <div className="container">
+                <section className="user-container">
+                    <div id="user-avatar-container" className="user-container__avatar-container">
+                        <img className="shadow" id="user-avatar" src="./img/c46888f9135c3b440f5d9060a345b35e.png" alt="No image yet" />
                     </div>
-                    <div id="current-day-container" class="user-container__selected-day-container">
+                    <div id="current-day-container" className="user-container__selected-day-container">
                         <p>Select day</p>
                     </div>
-                    <div class="user-container__user-challenges-container">
+                    <div className="user-container__user-challenges-container">
                         <div id="user-challenges-container"
-                            class="user-container__user-challenges-container__user-chalenges shadow">
+                            className="user-container__user-challenges-container__user-chalenges shadow">
                         </div>
                     </div>
-                    <div class="user-container__button-container">
-                        <button id="add-challenge-button" class="wide-btn shadow">
+                    <div className="user-container__button-container">
+                        <button id="add-challenge-button" className="wide-btn shadow">
                             <ion-icon class="button-content" name="add-outline"></ion-icon>
                         </button>
                     </div>
                 </section>
-                <section class="calendar-container">
-                    <table class="calendar-container__days">
-                        <thead class="day-names">
+                <section className="calendar-container">
+                    <table className="calendar-container__days">
+                        <thead className="day-names">
                             <tr>
-                                <th class="day-names__day">Sunday</th>
-                                <th class="day-names__day">Monday</th>
-                                <th class="day-names__day">Tuesday</th>
-                                <th class="day-names__day">Wenesday</th>
-                                <th class="day-names__day">Thursday</th>
-                                <th class="day-names__day">Friday</th>
-                                <th class="day-names__day">Saturday</th>
+                                <th className="day-names__day">Sunday</th>
+                                <th className="day-names__day">Monday</th>
+                                <th className="day-names__day">Tuesday</th>
+                                <th className="day-names__day">Wenesday</th>
+                                <th className="day-names__day">Thursday</th>
+                                <th className="day-names__day">Friday</th>
+                                <th className="day-names__day">Saturday</th>
                             </tr>
                         </thead>
-                        <tbody id="days-table" class="day-values">
+                        <tbody id="days-table" className="day-values">
                         </tbody>
                     </table>
                 </section>
